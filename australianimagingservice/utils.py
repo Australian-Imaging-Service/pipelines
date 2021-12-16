96ded54aaa6752548de4ff23fc24d3078be816ff
Tom Close <tom.g.close@gmail.com>
2021-12-16 16:43:52 +1100

    added new modules for range of relevant BIDS apps

--- australianimagingservice/utils.py
+++ australianimagingservice/utils.py
@@ -1,29 +1,5 @@
 import docker
 
 
-def docker_image_executable(image_tag):
-    """Pulls a given Docker image tag and inspects the image to get its
-    entrypoint/cmd
 
-    Parameters
-    ----------
-    image_tag : str
-        Docker image tag
-
-    Returns
-    -------
-    str
-        The entrypoint or default command of the Docker image
-    """
-    dc = docker.from_env()
-
-    dc.images.pull(image_tag)
-
-    image_attrs = dc.api.inspect_image(image_tag)['Config']
-
-    executable = image_attrs['Entrypoint']
-    if executable is None:
-        executable = image_attrs['Cmd']
-
-    return executable
     
