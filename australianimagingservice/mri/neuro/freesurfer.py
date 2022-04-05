ff4a39d2f34d79f3c25ea31119a91a8073b4496e
David McFarlane <54093156+Prepultrue@users.noreply.github.com>
2022-04-05 11:57:34 +1000

    Re-add pipeline python files containing BidsApp instances
    (cherry picked from commit bc376155374817f4b546348cac103d59d2b1e2a2)

--- australianimagingservice/mri/neuro/freesurfer.py
+++ australianimagingservice/mri/neuro/freesurfer.py
@@ -1,42 +1,18 @@
-from arcana.tasks.bids import BidsApp
-from arcana.data.spaces.medimage import Clinical
 from arcana.data.formats.common import Directory
 from arcana.data.formats.medimage import NiftiGzX
+from arcana.data.stores.bids import BidsApp
 
-
-AIS_VERSION = '0.1'
-VERSION = ''
-
-BIDS_INPUTS = [('T1w', NiftiGzX, 'anat/T1w'),
-               ('T2w', NiftiGzX, 'anat/T2w')]
-BIDS_OUTPUTS = [('freesurfer', Directory)]
-BIDS_PARAMETERS = []
-
-docker_image = f":{VERSION}"
-
-
-spec = {
-    'package_name': "freesurfer",
-    'description': (
-        ""),
-    'commands': [
-        {'pydra_task': 'task',  # Name of Pydra task preceded by module path, e.g. pydra.tasks.fsl.preprocess.fast:FAST. Module path can be omitted if defined in current module
-         'inputs': [i[:2] for i in BIDS_INPUTS],
-         'outputs': [o[:2] for o in BIDS_OUTPUTS],
-         'parameters': [p[0] for p in BIDS_PARAMETERS]}],
-    'version': AIS_VERSION,
-    'pkg_version': VERSION,
-    'packages': [],  # [('dcm2niix', '1.0.20201102')],
-    'python_packages': [],
-    'base_image': docker_image,
-    'maintainer': 'thomas.close@sydney.edu.au',
-    'info_url': '',
-    'frequency': 'session'}
-
+VERSION = ""
 
 task = BidsApp(
-    app_name=spec['package_name'],
-    image=docker_image,
-    executable='',  # Extracted using `docker_image_executable(docker_image)`
-    inputs=BIDS_INPUTS,
-    outputs=BIDS_OUTPUTS)
+    app_name="freesurfer",
+    image=f":{VERSION}",
+    executable="",  # Extracted using `docker_image_executable(docker_image)`
+    inputs=[
+        ("T1w", NiftiGzX, "anat/T1w"),
+        ("T2w", NiftiGzX, "anat/T2w"),
+    ],
+    outputs=[
+        ("freesurfer", Directory),
+    ],
+)
--- australianimagingservice/mri/neuro/mriqc.py
+++ australianimagingservice/mri/neuro/mriqc.py
@@ -1,43 +1,19 @@
-from arcana.tasks.bids import BidsApp
 from arcana.data.formats.common import Directory
 from arcana.data.formats.medimage import NiftiGzX
+from arcana.data.stores.bids import BidsApp
 
-
-VERSION = '0.16.1'
-
-BIDS_INPUTS = [('T1w', NiftiGzX, 'anat/T1w'),
-               ('T2w', NiftiGzX, 'anat/T2w'),
-               ('fMRI', NiftiGzX, 'func/bold')]
-BIDS_OUTPUTS = [('mriqc', Directory, None)]
-BIDS_PARAMETERS = []
-
-docker_image = f"poldracklab/mriqc:{VERSION}"
-
+VERSION = "0.16.1"
 
 mriqc = BidsApp(
     app_name="mriqc",
-    image=docker_image,
-    executable='mriqc',  # Extracted using `docker_image_executable(docker_image)`
-    inputs=BIDS_INPUTS,
-    outputs=BIDS_OUTPUTS)
-
-
-spec = {
-    'commands': [
-        {'pydra_task': __name__ + ':mriqc',
-         'inputs': [i[:2] for i in BIDS_INPUTS],
-         'outputs': [o[:2] for o in BIDS_OUTPUTS],
-         'parameters': [p[0] for p in BIDS_PARAMETERS]}],
-         'description': (
-             "MRIQC extracts no-reference IQMs (image quality metrics) from "
-             "structural (T1w and T2w) and functional MRI (magnetic resonance "
-             "imaging) data."),
-    'pkg_version': VERSION,
-    'wrapper_version': None,    
-    'packages': [],
-    'python_packages': [],
-    'base_image': docker_image,
-    'authors': ['thomas.close@sydney.edu.au'],
-    'info_url': 'http://mriqc.readthedocs.io',
-    'frequency': 'session'}
-
+    image=f"poldracklab/mriqc:{VERSION}",
+    executable="mriqc",  # Extracted using `docker_image_executable(docker_image)`
+    inputs=[
+        ("T1w", NiftiGzX, "anat/T1w"),
+        ("T2w", NiftiGzX, "anat/T2w"),
+        ("fMRI", NiftiGzX, "func/bold"),
+    ],
+    outputs=[
+        ("mriqc", Directory, None),
+    ],
+)
