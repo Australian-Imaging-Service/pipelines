e9cf3c61abd955965f207632e863b16589faf3be
Tom Close <tom.g.close@gmail.com>
2022-05-16 14:49:10 +1000

    updated bids tests

--- conftest.py
+++ conftest.py
@@ -40,19 +40,23 @@ def work_dir():
     
 
 bids_apps_dir = Path(__file__).parent / 'pipeline-specs' / 'mri' / 'neuro' / 'bids'
+test_bids_data_dir = Path(__file__).parent / 'tests' / 'data' / 'mri' / 'neuro' / 'bids'
 
 bids_specs = [str(p.stem) for p in bids_apps_dir.glob('*.yaml')]
 
 
 @pytest.fixture(params=bids_specs)
-def bids_app_spec_path(request):
-    return str(bids_apps_dir / request.param) + '.yaml'
+def bids_app_spec_and_project(run_prefix, request):
+    bids_app_name = request.param
+    project_id = make_project_name(bids_app_name, run_prefix=run_prefix)
+    upload_test_dataset_to_xnat(project_id, test_bids_data_dir / bids_app_name)
+    return bids_apps_dir / (bids_app_name+ '.yaml'), project_id
 
 
 @pytest.fixture(scope='session')
-def xnat_host():
+def xnat_connect():
     xnat4tests.launch_xnat()
-    yield xnat4tests.config.XNAT_URI
+    yield xnat4tests.connect
     #xnat4tests.stop_xnat()
 
 
@@ -87,3 +91,41 @@ def cli_runner():
                                **kwargs)
         return result
     return invoke
+
+TEST_SUBJECT_LABEL = 'TESTSUBJ'
+TEST_SESSION_LABEL = 'TESTSUBJ_01'
+
+
+def make_project_name(dataset_name: str, run_prefix: str=None):
+    return (run_prefix if run_prefix else '') + dataset_name
+
+
+def upload_test_dataset_to_xnat(project_id: str, source_data_dir: Path):
+    """
+    Creates dataset for each entry in dataset_structures
+    """
+
+    with xnat4tests.connect() as login:
+        login.put(f'/data/archive/projects/{project_id}')
+    
+    with xnat4tests.connect() as login:
+        xproject = login.projects[project_id]
+        xclasses = login.classes
+        xsubject = xclasses.SubjectData(label=TEST_SUBJECT_LABEL,
+                                        parent=xproject)
+        xsession = xclasses.MrSessionData(label=TEST_SESSION_LABEL,
+                                          parent=xsubject)
+        for scan_path in source_data_dir.iterdir():
+            # Create scan
+            xscan = xclasses.MrScanData(id=scan_path.stem, type=scan_path.stem,
+                                        parent=xsession)
+            
+            for resource_path in scan_path.iterdir():
+
+                # Create the resource
+                xresource = xscan.create_resource(resource_path.stem)
+                # Create the dummy files
+                xresource.upload_dir(resource_path, method='tar_file')
+
+        # Populate metadata from DICOM headers
+        # login.put(f'/data/experiments/{xsession.id}?pullDataFromHeaders=true')
--- tests/test_bids_apps.py
+++ tests/test_bids_apps.py
@@ -1,72 +1,42 @@
-import time
 import json
-from pathlib import Path
-import xnat4tests
-from arcana.test.utils import show_cli_trace
-from arcana.core.utils import resolve_class
 from arcana.cli.deploy import build
+from arcana.core.utils import path2varname
+from arcana.test.utils import show_cli_trace
+from arcana.deploy.medimage.xnat import path2xnatname
 from arcana.core.deploy.utils import load_yaml_spec
 from arcana.test.stores.medimage.xnat import (
-    create_dataset_data_in_repo,
-    TestXnatDatasetBlueprint,
-    ResourceBlueprint,
-    ScanBlueprint,
     install_and_launch_xnat_cs_command)
 
 
-
-# def test_bids_app_build(bids_app_spec_path, cli_runner, work_dir):
-#     result = cli_runner(
-#         build,
-#         [bids_app_spec_path,
-#          'australianimagingservice',
-#          '--build_dir', str(work_dir),
-#          '--use-local-packages', '--raise-errors'])
-
-#     assert result.exit_code == 0, show_cli_trace(result)
+SKIP_BUILD = False
     
-    
-def test_bids_app(nifti_data, bids_app_spec_path, cli_runner, run_prefix, xnat_host):
+def test_bids_app(bids_app_spec_and_project, run_prefix, xnat_connect, cli_runner):
 
-    bids_app_spec_path = Path(bids_app_spec_path)
+    bids_app_spec_path, project_id = bids_app_spec_and_project
 
     build_dir = bids_app_spec_path.parent / '.build' / bids_app_spec_path.stem
     
     build_dir.mkdir(exist_ok=True, parents=True)
 
-    # result = cli_runner(
-    #     build,
-    #     [str(bids_app_spec_path),
-    #      'australianimagingservice',
-    #      '--build_dir', str(build_dir),
-    #      '--use-local-packages', '--raise-errors'])
+    if not SKIP_BUILD:
+        result = cli_runner(
+            build,
+            [str(bids_app_spec_path),
+            'australianimagingservice',
+            '--build_dir', str(build_dir),
+            '--use-local-packages', '--raise-errors'])
 
-    # assert result.exit_code == 0, show_cli_trace(result)
+        assert result.exit_code == 0, show_cli_trace(result)
     
     spec = load_yaml_spec(bids_app_spec_path)
     
     for cmd_spec in spec['commands']:
+
         cmd_name = cmd_spec['name']
-        scan_blueprints = []
-        for inpt in cmd_spec['inputs']:
-            format_loc = inpt['format']
-            format = resolve_class(format_loc, prefixes=['arcana.data.formats'])
-            resource_name = format_loc.split(':')[-1]
-            scan_blueprints.append(
-                ScanBlueprint(
-                    inpt['path'],
-                    [ResourceBlueprint(
-                        resource_name, format,
-                        [inpt['path'] + '.' + e for e in format.all_exts()])]))
-        
-        blueprint = TestXnatDatasetBlueprint([1, 1, 1], scan_blueprints, {}, [])
-        
-        create_dataset_data_in_repo(cmd_name, blueprint,
-                                    run_prefix=run_prefix, source_data=nifti_data)
         
         project_id = run_prefix + cmd_name
 
-        with xnat4tests.connect() as xlogin:
+        with xnat_connect() as xlogin:
 
             with open(build_dir / 'xnat_commands' / (cmd_name + '.json')) as f:
                 xnat_command = json.load(f)
@@ -76,7 +46,7 @@ def test_bids_app(nifti_data, bids_app_spec_path, cli_runner, run_prefix, xnat_h
             inputs_json = {}
 
             for inpt in cmd_spec['inputs']:
-                inputs_json[inpt['name']] = inpt['name']
+                inputs_json[path2xnatname(inpt['path'])] = path2varname(inpt['path'])
 
             # for pname in cmd_spec['parameters']:
             #     launch_json[pname] = pval                
@@ -96,55 +66,3 @@ def test_bids_app(nifti_data, bids_app_spec_path, cli_runner, run_prefix, xnat_h
             # for deriv in blueprint.derivatives:
             #     assert list(test_xsession.resources[deriv.name].files) == deriv.filenames
     
-    # blueprint = TestDatasetBlueprint(
-    # hierarchy=[Clinical.subject, Clinical.session],
-    # dim_lengths=[1, 1, 1],
-    # files=["anat/T1w.nii.gz", "anat/T1w.json", "anat/T2w.nii.gz", "anat/T2w.json",
-    #         "dwi/dwi.nii.gz", "dwi/dwi.json", "dwi/dwi.bvec", "dwi/dwi.bval"],
-    # expected_formats={
-    #     "anat/T1w": (NiftiGzX, ["T1w.nii.gz", "T1w.json"]),
-    #     "anat/T2w": (NiftiGzX, ["T2w.nii.gz", "T2w.json"]),
-    #     "dwi/dwi": (NiftiGzX, ["dwi.nii.gz", "dwi.json", "dwi.bvec", "dwi.bval"])},
-    # derivatives=[('file1', Clinical.session, Text, ['file1.txt']),
-    #                 ('file2', Clinical.session, Text, ['file2.txt'])])
-        
-    # dataset_path = work_dir / 'bids-dataset'
-    
-    # dataset = make_dataset(
-    #     dataset_path=dataset_path,
-    #     blueprint=blueprint,
-    #     source_data=nifti_sample_dir)
-
-    # dataset_id_str = f'file//{dataset_path}'
-    # # Start generating the arguments for the CLI
-    # # Add source to loaded dataset
-    # args = [dataset_id_str, 'a_bids_app', 'arcana.tasks.bids.app:bids_app',
-    #         '--plugin', 'serial',
-    #         '--work', str(work_dir),
-    #         '--configuration', 'executable', str(mock_bids_app_executable),
-    #         '--dataset_space', class_location(blueprint.space),
-    #         '--dataset_hierarchy', ','.join([str(l) for l in blueprint.hierarchy])]
-    # inputs_config = []
-    # for path, (format, _) in blueprint.expected_formats.items():
-    #     format_str = class_location(format)
-    #     args.extend(['--input', path2varname(path), format_str, path2varname(path), format_str])
-    #     inputs_config.append({'path': path, 'format': format_str})
-    # args.extend(['--configuration', 'inputs', json.dumps(inputs_config).replace('"', '\\"')])
-    # outputs_config = []
-    # for path, _, format, _ in blueprint.derivatives:
-    #     format_str = class_location(format)
-    #     args.extend(['--output', path, format_str, path2varname(path), format_str])
-    #     outputs_config.append({'path': path, 'format': format_str})
-    # args.extend(['--configuration', 'outputs', json.dumps(outputs_config).replace('"', '\\"')])
-    
-    # result = cli_runner(run_pipeline, args)
-    # assert result.exit_code == 0, show_cli_trace(result)
-    # # Add source column to saved dataset
-    # for fname in ['file1', 'file2']:
-    #     sink = dataset.add_sink(fname, Text)
-    #     assert len(sink) == reduce(mul, dataset.blueprint.dim_lengths)
-    #     for item in sink:
-    #         item.get(assume_exists=True)
-    #         with open(item.fs_path) as f:
-    #             contents = f.read()
-    #         assert contents == fname + '\n'
