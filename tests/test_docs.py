4ea2b56b2939ab034c04b40673d3483b3becc111
Tom Close <tom.g.close@gmail.com>
2022-12-12 22:52:15 +1100

    updated arg order in make

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,8 +1,8 @@
 from arcana.core.utils.testing import show_cli_trace
-from arcana.cli.deploy import build_docs
+from arcana.cli.deploy import make_docs
 
 
-def test_build_docs(bids_app_spec_path, cli_runner, work_dir):
-    result = cli_runner(build_docs, [bids_app_spec_path, str(work_dir)])
+def test_make_docs(bids_app_spec_path, cli_runner, work_dir):
+    result = cli_runner(make_docs, [bids_app_spec_path, str(work_dir)])
 
     assert result.exit_code == 0, show_cli_trace(result)
