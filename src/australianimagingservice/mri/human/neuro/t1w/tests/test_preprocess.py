35302467cfc7893ed596a48bbe01ec22ad1949ec
Tom Close <tom.g.close@gmail.com>
2024-10-22 15:05:26 +1100

    added test to load preprocess yaml

--- src/australianimagingservice/mri/human/neuro/t1w/tests/test_preprocess.py
+++ src/australianimagingservice/mri/human/neuro/t1w/tests/test_preprocess.py
@@ -1,4 +1,10 @@
+from pathlib import Path
+from pipeline2app.core import App
 from australianimagingservice.mri.human.neuro.t1w.preprocess import all_parcs
+
+PKG_DIR = (
+    Path(__file__).parent / ".." / ".." / ".." / ".." / ".." / ".." / ".."
+).resolve()
 
 
 def test_t1w_preprocess():
@@ -8,3 +14,18 @@ def test_t1w_preprocess():
         "/Users/tclose/Desktop/cache-dir",
         "/Users/tclose/Desktop/file1.txt",
     )
+
+
+def test_t1w_preprocess_yaml(tmp_path: Path):
+    app = App.load(
+        PKG_DIR
+        / "specs"
+        / "australian-imaging-service"
+        / "mri"
+        / "human"
+        / "neuro"
+        / "t1w"
+        / "preprocess.yaml"
+    )
+
+    app.make(build_dir=tmp_path, generate_only=True)
