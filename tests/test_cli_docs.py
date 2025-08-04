3d60fd03fd81fddc6fac2bc194618a1d17e94b86
Thomas G. Close <tom.g.close@gmail.com>
2025-08-04 10:30:43 +1000

    moved tests inside pytestable functions

--- tests/test_cli_docs.py
+++ tests/test_cli_docs.py
@@ -7,16 +7,18 @@ PKG_PATH = Path(__file__).parent.parent.absolute()
 
 runner = CliRunner()
 
-os.chdir(PKG_PATH)
 
-results = runner.invoke(
-    make_docs,
-    [
-        "australian-imaging-service",
-        "docs/pipelines",
-        "--flatten",
-        "--default-data-space",
-        "arcana.common:Clinical",
-    ],
-    catch_exceptions=False,
-)
+def test_make_docs():
+    os.chdir(PKG_PATH)
+
+    results = runner.invoke(
+        make_docs,
+        [
+            "australian-imaging-service",
+            "docs/pipelines",
+            "--flatten",
+            "--default-data-space",
+            "arcana.common:Clinical",
+        ],
+        catch_exceptions=False,
+    )
--- tests/test_make.py
+++ tests/test_make.py
@@ -7,14 +7,16 @@ PKG_PATH = Path(__file__).parent.parent.absolute()
 
 runner = CliRunner()
 
-results = runner.invoke(
-    make,
-    [
-        f"{PKG_PATH}/australian-imaging-service/mri/human/neuro/bidsapps/fmriprep.yaml",
-        "xnat:XnatApp",
-        "--raise-errors",
-    ],
-    catch_exceptions=False,
-)
 
-print(show_cli_trace(results))
+def test_make_fmriprep():
+    results = runner.invoke(
+        make,
+        [
+            f"{PKG_PATH}/australian-imaging-service/mri/human/neuro/bidsapps/fmriprep.yaml",
+            "xnat:XnatApp",
+            "--raise-errors",
+        ],
+        catch_exceptions=False,
+    )
+
+    print(show_cli_trace(results))
