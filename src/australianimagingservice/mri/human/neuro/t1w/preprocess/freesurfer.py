291c0f952149cb23418c0ccf83b964621b9052c6
Thomas G. Close <tom.g.close@gmail.com>
2025-08-20 10:08:17 +1000

    reworking t1w pipeline into new pydra syntax

--- src/australianimagingservice/mri/human/neuro/t1w/preprocess/freesurfer.py
+++ src/australianimagingservice/mri/human/neuro/t1w/preprocess/freesurfer.py
@@ -1,6 +1,6 @@
 from pydra.compose import shell
 from fileformats.generic import File, Directory
-from fileformasts.medimage import NiftiGz
+from fileformats.medimage import NiftiGz
 
 ###########################
 # mri_surf2surf spec info #
@@ -12,7 +12,7 @@ class MRISurf2Surf(shell.Task["MRISurf2Surf.Outputs"]):
     source_subject_id: str = shell.arg(
         help="source subject",
         argstr="--srcsubject",
-    )
+    )  # pyright: ignore[reportAssignmentType]
     target_subject_id: str = shell.arg(
         help="target subject",
         argstr="--trgsubject",
@@ -31,15 +31,13 @@ class MRISurf2Surf(shell.Task["MRISurf2Surf.Outputs"]):
     )
 
     class Outputs:
-        target_annotation_file: File = shell.out(
+        target_annotation_file: File = shell.outarg(
             help="path of file in which to store output values",
             argstr="--tval",
-            mandatory=True,
         )
-        target_subject_id: Directory = shell.out(
+        target_subject_id: Directory = shell.outarg(
             help="target subject",
             argstr="--trgsubject",
-            mandatory=False,
         )
 
     # ############################
@@ -52,7 +50,6 @@ class MRIAparc2Aseg(shell.Task["MRIAparc2Aseg.Outputs"]):
     subject: Directory = shell.arg(
         help="Name of the subject as found in the SUBJECTS_DIR",
         argstr="--s",
-        mandatory=True,
     )
     old_ribbon: bool = shell.arg(
         help="use mri/hemi.ribbon.mgz as a mask for the cortex",
@@ -67,15 +64,9 @@ class MRIAparc2Aseg(shell.Task["MRIAparc2Aseg.Outputs"]):
     annotname: str = shell.arg(
         help="Use annotname surface annotation. By default, uses ?h.aparc.annot. With this option, it will load ?h.annotname.annot. The output file will be set to annotname+aseg.mgz, but this can be changed with --o. Note: running --annot aparc.a2009s is NOT the same as running --a2009s. The index numbers will be different.",
     )
-    volfile: File = shell.arg(
-        help="Full path of file to save the output segmentation in. Default is mri/aparc+aseg.mgz",
-        argstr="--o",
-        path_template="volfile.nii.gz",
-        default="mri/aparc+aseg.mgz",
-    )
 
     class Outputs:
-        volfile: NiftiGz = shell.out(
+        volfile: NiftiGz = shell.outarg(
             help="Full path of file to save the output segmentation in. Default is mri/aparc+aseg.mgz",
             argstr="--o",
             path_template="volfile.nii.gz",
@@ -92,26 +83,18 @@ class MRILabel2Volume(shell.Task["MRILabel2Volume.Outputs"]):
     seg: File = shell.arg(
         help="segpath : segmentation",
         argstr="--seg",
-        mandatory=True,
     )
     temp: File = shell.arg(
         help="tempvolid : output template volume",
         argstr="--temp",
-        mandatory=True,
     )
     regheader: File = shell.arg(
         help="volid : label template volume (needed with --label or --annot)",
         argstr="--regheader",
-        mandatory=True,
-    )
-    output: str = shell.arg(
-        help="volid : output volume",
-        argstr="--o",
-        path_template="label2vol_out1.nii.gz",
     )
 
     class Outputs:
-        output: NiftiGz = shell.out(
+        output: NiftiGz = shell.outarg(
             help="volid : output volume",
             argstr="--o",
             path_template="label2vol_out1.nii.gz",
