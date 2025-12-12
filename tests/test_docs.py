af6e554fde9c89a89b168a050ab76609dd6cf522
Thomas G. Close <tom.g.close@gmail.com>
2025-12-12 13:57:17 +1100

    generalised doc unittest

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -3,7 +3,7 @@ from pydra2app.core.cli import make_docs
 from conftest import specs_dir
 
 
-def test_make_docs(bids_app_spec_path, cli_runner, work_dir):
+def test_make_docs(cli_runner, work_dir):
     result = cli_runner(make_docs, [str(specs_dir), str(work_dir)])
 
     assert result.exit_code == 0, show_cli_trace(result)
