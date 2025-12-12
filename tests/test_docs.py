d4a00567eb8f5095bdea69ce9b208d5c254c455d
Thomas G. Close <tom.g.close@gmail.com>
2025-12-12 12:44:15 +1100

    updated docs test to target whole library

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,8 +1,9 @@
 from frametree.core.utils import show_cli_trace
 from pydra2app.core.cli import make_docs
+from conftest import specs_dir
 
 
 def test_make_docs(bids_app_spec_path, cli_runner, work_dir):
-    result = cli_runner(make_docs, [bids_app_spec_path, str(work_dir)])
+    result = cli_runner(make_docs, [str(specs_dir), str(work_dir)])
 
     assert result.exit_code == 0, show_cli_trace(result)
