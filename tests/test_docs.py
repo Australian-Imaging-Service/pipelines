08c607604e198447b4015d76b1cdbe7bf8ced538
Thomas G. Close <tom.g.close@gmail.com>
2025-12-17 15:40:53 +1100

    dropped redudant docs test

--- tests/test_docs.py
+++ tests/test_docs.py
@@ -1,9 +1,29 @@
-from frametree.core.utils import show_cli_trace
+import os
+from pathlib import Path
+from click.testing import CliRunner
 from pydra2app.core.cli import make_docs
-from conftest import specs_dir
+from frametree.core.utils import show_cli_trace
+
+PKG_PATH = Path(__file__).parent.parent.absolute()
+
+runner = CliRunner()
 
 
-def test_make_docs(cli_runner, work_dir):
-    result = cli_runner(make_docs, [str(specs_dir), str(work_dir)])
+def test_make_docs(tmp_path: Path) -> None:
+    os.chdir(PKG_PATH)
 
-    assert result.exit_code == 0, show_cli_trace(result)
+    results = runner.invoke(
+        make_docs,
+        [
+            str(PKG_PATH / "specs" / "australian-imaging-service"),
+            str(tmp_path),
+            "--flatten",
+            "--default-axes",
+            "medimage",
+        ],
+        catch_exceptions=False,
+    )
+
+    assert results.exit_code == 0, show_cli_trace(results)
+    docs = list(tmp_path.glob("**/*.md"))
+    assert len(docs) > 0
