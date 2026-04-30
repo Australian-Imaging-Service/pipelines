c9f73f0adbf3aa505fc2d44e1bc46e1a85148652
arkiev <arkiev.dsouza@sydney.edu.au>
2026-04-30 11:39:07 +1000

    regridding DWI to 5TT voxel size during registration, updated epi_reg to use norm.mgz derivative

--- src/australianimagingservice/_version.py
+++ src/australianimagingservice/_version.py
@@ -18,7 +18,7 @@ version_tuple: tuple[int | str, ...]
 commit_id: str | None
 __commit_id__: str | None
 
-__version__ = version = '2026.3.1.dev2+g6a9b8cbe1'
-__version_tuple__ = version_tuple = (2026, 3, 1, 'dev2', 'g6a9b8cbe1')
+__version__ = version = '2026.3.1.dev17+g6d2eb20a5.d20260424'
+__version_tuple__ = version_tuple = (2026, 3, 1, 'dev17', 'g6d2eb20a5.d20260424')
 
 __commit_id__ = commit_id = None
