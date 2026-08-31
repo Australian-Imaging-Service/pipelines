462950354c6c8a8a2f27f95243234bb148dd7b51
arkiev <arkiev.dsouza@sydney.edu.au>
2026-08-31 14:16:28 +1000

    reverted submission script to working version prior to output edits

--- tests/run_t1_test.sh
+++ tests/run_t1_test.sh
@@ -11,11 +11,12 @@ cd /home/ubuntu/git/pydra-tasks-fsl
 git pull
 pip install -e .
 
-
 cd /home/ubuntu/git/pipelines
 # CHECK WHICH BRANCH YOU ARE ON AND PULL THE LATEST CHANGES
 git pull
-pip install -e ".[test]"
+pip install -e .
+pip install -I -r requirements.txt
+pip install --upgrade pytest
 
-#nohup pytest tests/test_t1_preprocess.py > tests/test_t1_preprocess_OUTPUT.txt &
-nohup pytest tests/test_dwi_preprocess.py > tests/test_dwi_preprocess_OUTPUT.txt &
+nohup pytest tests/test_t1_preprocess.py > tests/test_t1_preprocess_OUTPUT.txt &
+# nohup pytest tests/test_dwi_preprocess.py > tests/test_dwi_preprocess_OUTPUT.txt &
